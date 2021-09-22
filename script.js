e89d6ed065caf9be774577121b401e2f7f14152b
JDLanata <87950670+JDLanata@users.noreply.github.com>
2021-09-22 16:43:07 -0700

    created initial variables

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+
+// Array holding characters
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specail = ["!", "#", "$", "%", "&", "*", "+", "/", "<", "=", ">", "?", "@", "|"]
+
+//question prompts
+var numLong = prompt("How long do you want your passowrd to be?")
+var upperChoice = confirm("Do you want uppercase letters?")
+var lowerChoice = confirm("Do you want lowercase letters?")
+var specialChoice = confirm("Do you want specail characters?")
+var numChoice = confirm("Do you want Numbers?")
+
+//Random number slectors for character choices
+if (upperChoice == true) {
+  var selectUpper = Math.floor(Math.random() * 26);
+} else {
+
+}
+
+if (lowerChoice = true) {
+  var selectLower = Math.floor(Math.random() * 26);
+} else {
+
+}
+
+if (specialChoice = true) {
+ var selectSpecial = Math.floor(Math.random()*14);
+} else {
+
+}
+
+if (numChoice = true) {
+ var selectNum = Math.floor(Math.random()*10);
+} else {
+
+}
+//creating funtion to generate password
+function generatePassword() {
+
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,3 +55,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
